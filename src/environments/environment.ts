e654068e09e4762da6a3939bbe0d7ac65c5f4466
Clément Delestre <clementdelestre@gmail.com>
2021-12-11 15:48:15 +0100

    update checker

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import pkg from '../../package.json';
+
 export const environment = {
-  production: false
+  production: false,
+  appVersion: pkg.version + '-dev',
 };
 
 /*
